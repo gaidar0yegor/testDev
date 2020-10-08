d366ec759b899b42bda3be360f61e5734b90481c
Fleury VUADIAMBO <matanu.fv@gmail.com>
2020-10-08 12:04:58 +0200

    Modification templates et controller utilisateurs et projet

--- src/Controller/ProjetsController.php
+++ src/Controller/ProjetsController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
+use App\Repository\ProjetsRepository;
+
 
 class ProjetsController extends AbstractController
 {
     /**
      * @Route("/projets", name="projets")
      */
-    public function index()
+    public function index(ProjetsRepository $pr)
     {
+        $liste_projets = $pr->findAll();
         return $this->render('projets/index.html.twig', [
-            'controller_name' => 'ProjetsController',
+            'liste_projets' => $liste_projets,
         ]);
     }
 }
--- src/Controller/UtilisateursFoController.php
+++ src/Controller/UtilisateursFoController.php
@@ -4,16 +4,44 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
+use App\Repository\UsersRepository;
+
+
 
 class UtilisateursFoController extends AbstractController
 {
     /**
      * @Route("/utilisateurs/fo", name="utilisateurs_fo")
      */
-    public function index()
+    public function index(UsersRepository $ur)
     {
+        $liste_utilisateurs = $ur->findAll();
         return $this->render('utilisateurs_fo/index.html.twig', [
-            'controller_name' => 'UtilisateursFoController',
+            'liste_utilisateurs' => $liste_utilisateurs,
         ]);
     }
+
+    /**
+     * @Route("/utilisateurs/fo/modifier/{id}", name="utilisateurs_fo_modifier", requirements={"id"="\d+"})
+     */
+    // public function modifier(Request $rq, EntityManager $em, ArtistRepository $ar, $id)
+    // {
+    //     // Modifier un utilisateur existant
+    //     // récupérer l'utilisateur avec son id, l'afficher dans le formulaire et enregistrer les modifications dans la base de données
+    //     $utilisateurAmodifier = $ar->find($id);
+    //     $formArtiste = $this->createForm(ArtistType::class, $utilisateurAmodifier);
+    //     $formArtiste->handleRequest($rq);
+    //     if($formArtiste->isSubmitted() && $formArtiste->isValid()){
+    //         // $em->persist($utilisateurAmodifier);
+    //         $em->flush();
+    //         $this->addFlash("success", "Les informations de l'artiste ont été modifiées");
+    //         return $this->redirectToRoute("artist");
+    //     }
+    //     return $this->render("artist/form.html.twig", [ 
+    //         "form" => $formArtiste->createView(), 
+    //         "bouton" => "Modifier",
+    //         "titre" => "Modification de l'artiste n°$id" 
+    //     ]);
+    // }
 }
