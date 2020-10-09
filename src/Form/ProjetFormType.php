ca0b84ba6cac968b3d09b5e6968ef260da6115a6
Fleury VUADIAMBO <matanu.fv@gmail.com>
2020-10-09 15:28:08 +0200

    Modification templates et type  projet  OK

--- src/Form/ProjetFormType.php
+++ src/Form/ProjetFormType.php
@@ -7,25 +7,61 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 
 class ProjetFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('acronyme')    
-            ->add('titre')
-            ->add('resume')
-            ->add('date_debut')
-            ->add('date_fin')
+            ->add('acronyme', NumberType::class, [
+                "label" => "Acronyme",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])        
+            ->add('titre', TextType::class, [
+                "label" => "Titre",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('resume', TextareaType::class, [
+                "label" => "Résumé",
+                "attr" => [
+                    "rows" => 9,
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('date_debut', DateType::class, [  
+                'label' => 'Date de début',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('date_fin', DateType::class, [
+                'label' => 'Date de fin',
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
             //->add('statut_rdi')
-            ->add('projet_interne')
-            ->add('projet_collaboratif')
-            ->add('projet_ppp')
+            ->add('projet_interne', CheckboxType::class, [
+                'label' => 'Projet en collaboration avec au moins un partenaire externe',
+            ])
+            ->add('projet_collaboratif', CheckboxType::class, [
+                'label' => 'Projet en collaboration avec un partenaire universitaire',               
+            ])
+            ->add('projet_ppp', CheckboxType::class, [
+                'label' => 'Projet réalisé en interne par la société (avec ou sans prestataires)',               
+            ])
             //->add('statuts_projet')
             ->add('ajouter', SubmitType::class, [
                 "label" => "Ajouter",
@@ -42,3 +78,10 @@ class ProjetFormType extends AbstractType
         ]);
     }
 }
+
+// ->add('acronyme', [
+//     "label" => "Acronyme",
+//     "attr" => [
+//         "class" => "form-control"
+//     ]
+// ])    
