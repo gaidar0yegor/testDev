134c817730a178d55cbb22406d0057030b415f52
Fleury VUADIAMBO <matanu.fv@gmail.com>
2020-11-05 11:22:28 +0100

    Liste des fichiers -> Ajouter un fichier -> Form

--- src/Form/UploadType.php
+++ src/Form/UploadType.php
@@ -3,11 +3,11 @@
 namespace App\Form;
 
 use App\Entity\FichierProjet;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UploadType extends AbstractType
 {
