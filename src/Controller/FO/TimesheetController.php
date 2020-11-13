fd95c03e33d794b61d141ecb9e3ba4e90362e5d0
Julien Maulny <julien.maulny@protonmail.com>
2020-11-13 16:25:59 +0100

    Génération de feuilles de temps multiple, sur une durée et plusieurs utilisateurs

--- src/Controller/FO/TimesheetController.php
+++ src/Controller/FO/TimesheetController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller\FO;
 
-use App\Entity\Cra;
-use App\Repository\CraRepository;
+use App\DTO\FilterTimesheet;
+use App\Entity\User;
+use App\Exception\MonthOutOfRangeException;
+use App\Exception\TimesheetException;
+use App\Form\FilterTimesheetType;
+use App\Repository\UserRepository;
+use App\Service\DateMonthService;
 use App\Service\TimesheetCalculator;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Knp\Snappy\Pdf;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,45 +24,99 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class TimesheetController extends AbstractController
 {
+    private Pdf $pdf;
+
+    public function __construct(Pdf $pdf)
+    {
+        $this->pdf = $pdf;
+    }
+
     /**
      * @Route(
      *      "/feuille-de-temps/generer",
      *      name="timesheet_generate"
      * )
      */
-    public function generate()
-    {
-        return $this->render('timesheet/generate.html.twig');
+    public function generate(
+        Request $request,
+        TimesheetCalculator $timesheetCalculator,
+        UserRepository $userRepository
+    ) {
+        $users = $userRepository->findBySameSociete($this->getUser());
+        $filter = new FilterTimesheet();
+        $filter->setUsers($users);
+        $form = $this->createForm(FilterTimesheetType::class, $filter);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $timesheets = $timesheetCalculator->generateMultipleTimesheets($filter);
+
+            $sheetHtml = $this->renderView('timesheet/pdf/pdf.html.twig', [
+                'timesheets' => $timesheets,
+            ]);
+
+            if ('html' === $filter->getFormat()) {
+                return new Response($sheetHtml);
+            }
+
+            return $this->createPdfResponse($sheetHtml);
+        }
+
+        return $this->render('timesheet/generate.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
+     * Génère une unique feuille de temps.
+     *
      * @Route(
-     *      "/feuille-de-temps-{id}.{format}",
+     *      "/feuille-de-temps-{year}-{month}-{userId}.{format}",
      *      requirements={"format"="(html|pdf)"},
      *      name="timesheet_sheet"
      * )
+     *
+     * @ParamConverter("user", options={"id" = "userId"})
      */
-    public function sheet(string $format, Pdf $pdf, TimesheetCalculator $timesheetCalculator, CraRepository $cr)
-    {
+    public function sheet(
+        string $year,
+        string $month,
+        User $user,
+        string $format,
+        DateMonthService $dateMonthService,
+        TimesheetCalculator $timesheetCalculator
+    ) {
+        try {
+            $date = $dateMonthService->getMonthFromYearAndMonth($year, $month);
+            $timesheet = $timesheetCalculator->generateTimesheet($user, $date);
+        } catch (MonthOutOfRangeException $e) {
+            throw $this->createNotFoundException($e->getMessage());
+        } catch (TimesheetException $e) {
+            throw $this->createNotFoundException($e->getMessage());
+        }
+
         $sheetHtml = $this->renderView('timesheet/pdf/pdf.html.twig', [
-            //'timesheet' => $timesheetCalculator->generateTimesheet($cra),
-            'timesheets' => array_map(function ($cra) use ($timesheetCalculator) {
-                return $timesheetCalculator->generateTimesheet($cra);
-            }, $cr->findAll()),
+            'timesheet' => $timesheet,
         ]);
 
         if ('html' === $format) {
             return new Response($sheetHtml);
         }
 
+        return $this->createPdfResponse($sheetHtml);
+    }
+
+    private function createPdfResponse(string $htmlContent, string $filename = 'feuille-de-temps.pdf'): PdfResponse
+    {
         $options = [
             'margin-top'    => 15,
             'margin-right'  => 15,
             'margin-bottom' => 15,
             'margin-left'   => 15,
-            'orientation'   => 'landscape',
+            //'orientation'   => 'landscape',
         ];
 
-        return new PdfResponse($pdf->getOutputFromHtml($sheetHtml, $options), 'timesheet.pdf');
+        return new PdfResponse($this->pdf->getOutputFromHtml($htmlContent, $options), $filename);
     }
 }
--- src/Service/TimesheetCalculator.php
+++ src/Service/TimesheetCalculator.php
@@ -2,27 +2,53 @@
 
 namespace App\Service;
 
+use App\DTO\FilterTimesheet;
 use App\DTO\Timesheet;
 use App\DTO\TimesheetProjet;
 use App\Entity\Cra;
 use App\Entity\ProjetParticipant;
 use App\Entity\TempsPasse;
 use App\Entity\User;
+use App\Exception\TimesheetException;
+use App\Repository\CraRepository;
 use App\Role;
-use ArrayAccess;
-use SplObjectStorage;
 
 class TimesheetCalculator
 {
     private ParticipantService $participantService;
 
-    public function __construct(ParticipantService $participantService)
-    {
+    private CraRepository $craRepository;
+
+    private DateMonthService $dateMonthService;
+
+    public function __construct(
+        ParticipantService $participantService,
+        CraRepository $craRepository,
+        DateMonthService $dateMonthService
+    ) {
         $this->participantService = $participantService;
+        $this->craRepository = $craRepository;
+        $this->dateMonthService = $dateMonthService;
     }
 
-    public function generateTimesheet(Cra $cra): Timesheet
+    public function generateTimesheet(User $user, \DateTime $month): Timesheet
     {
+        $this->dateMonthService->normalize($month);
+
+        $cra = $this->craRepository->findOneBy([
+            'user' => $user,
+            'mois' => $month,
+        ]);
+
+        if (null === $cra) {
+            throw new TimesheetException(sprintf(
+                'Impossible de générer la feuille de temps :'
+                .' l\'utilisateur "%s" n\'a pas rempli ses jours de congés du mois %s.',
+                $user->getEmail(),
+                $month->format('Y/m')
+            ));
+        }
+
         $participations = $this->participantService->getProjetParticipantsWithRole(
             $cra->getUser()->getProjetParticipants(),
             Role::CONTRIBUTEUR
@@ -58,6 +84,31 @@ class TimesheetCalculator
             $timesheetProjets,
             $totalJours
         );
+    }
+
+    /**
+     * @return Timesheet[]
+     */
+    public function generateMultipleTimesheets(FilterTimesheet $filter): array
+    {
+        $from = $filter->getFrom();
+        $to = $filter->getTo();
+
+        $this->dateMonthService->normalize($from);
+        $this->dateMonthService->normalize($to);
+
+        $timesheets = [];
+
+        foreach ($filter->getUsers() as $user) {
+            for ($month = $from; $month <= $to; $month = $this->dateMonthService->getNextMonth($month)) {
+                try {
+                    $timesheets[] = $this->generateTimesheet($user, $month);
+                } catch (TimesheetException $e) {
+                }
+            }
+        }
+
+        return $timesheets;
     }
 
     private function getTempsPassesOnProjet(User $user, ProjetParticipant $projetParticipant): ?TempsPasse
