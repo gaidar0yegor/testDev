d45be416ed97b1d4e8f075b0fa621e9b10844f12
Julien Maulny <julien.maulny@protonmail.com>
2020-11-27 11:08:34 +0100

    dashboard: indique a l'utilisateur si il est a jour avec ses temps saisis et absences

---
+++ public/js/dashboard/temps-saisis.js
@@ -0,0 +1,65 @@
+(function (window, fetch) {
+    'use strict';
+
+    fetch('/api/dashboard/temps-du-mois')
+        .then(response => response.json())
+        .then(cra => {
+            let text = '';
+            let userValidated = true;
+
+            if (!cra.hasTempsPasses) {
+                text += `
+                    <p class="text-success">
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                        Ce mois-ci, vous n'avez pas de temps à saisir car vous n'étiez contributeur sur aucun projet.
+                    </p>
+                `;
+            } else {
+                if (cra.isTempsPassesSubmitted) {
+                    text += `
+                        <p class="text-success">
+                            <i class="fa fa-check" aria-hidden="true"></i>
+                            Vous avez enregistré ce mois le ${cra.tempsPassesModifiedAt}.
+                        </p>
+                    `;
+                } else {
+                    userValidated = false;
+                    text += `
+                        <p class="text-warning">
+                            <i class="fa fa-times" aria-hidden="true"></i>
+                            Vous devriez enregistrer vos temps passés de ce mois.
+                        </p>
+                    `;
+                }
+            }
+
+
+            if (cra.isCraSubmitted) {
+                text += `
+                    <p class="text-success">
+                        <i class="fa fa-check" aria-hidden="true"></i>
+                        Vous avez enregistré ce mois le ${cra.craModifiedAt}.
+                    </p>
+                `;
+            } else {
+                userValidated = false;
+                text += `
+                    <p class="text-warning">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                        Vous devriez enregistrer vos absences de ce mois.
+                    </p>
+                `;
+            }
+
+            const mesTempsRappel = window['mes-temps-rappel'];
+
+            const icon = userValidated
+                ? 'calendar-check-o'
+                : 'calendar-times-o'
+            ;
+
+            mesTempsRappel.querySelector('.temps-messages').innerHTML = text;
+            mesTempsRappel.querySelector('i.fa').className = 'fa fa-'+icon;
+        })
+    ;
+})(window, fetch);
---
+++ src/Controller/API/DashboardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Service\CraService;
+use App\Service\DateMonthService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/dashboard")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * Retourne si l'utilisateur est à jour dans la saisie de ses temps.
+     *
+     * @Route(
+     *      "/temps-du-mois",
+     *      methods={"GET"},
+     *      name="api_dashboard_temps_du_mois"
+     * )
+     */
+    public function getTempsDuMois(CraService $craService, DateMonthService $dateMonthService)
+    {
+        $cra = $craService->loadCraForUser(
+            $this->getUser(),
+            $dateMonthService->getCurrentMonth()
+        );
+
+        return new JsonResponse([
+            'isCraSubmitted' => $cra->isCraSubmitted(),
+            'isTempsPassesSubmitted' => $cra->isTempsPassesSubmitted(),
+            'hasTempsPasses' => $cra->hasTempsPasses(),
+
+            'craModifiedAt' => null === $cra->getCraModifiedAt()
+                ? null
+                : $cra->getCraModifiedAt()->format('d M Y')
+            ,
+
+            'tempsPassesModifiedAt' => null === $cra->getTempsPassesModifiedAt()
+                ? null
+                : $cra->getTempsPassesModifiedAt()->format('d M Y')
+            ,
+        ]);
+    }
+}
