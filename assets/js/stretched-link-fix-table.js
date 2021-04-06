c423383e4958f10db574fa405a089828eb39f70e
Julien Maulny <julien.maulny@protonmail.com>
2021-04-06 15:32:30 +0200

    Répare le click sur les lignes des tables (projets, utilisateurs)

---
+++ assets/js/stretched-link-fix-table.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+/**
+ * Hard fix class="stretched-link" on table rows
+ * because it does not work on chrome.
+ * To use it:
+ *      - add class="stretched-link-table-row" on <table>
+ *      - then class="stretched-link" on "td a" element
+ *
+ * {@see https://github.com/twbs/bootstrap/issues/28608}
+ */
+
+$('table.stretched-link-table-row tbody td a.stretched-link').each((i, a) => {
+    const $a = $(a);
+    const $aEmpty = $(a).clone().removeAttr('title').text('');
+    const $td = $a.closest('tr').find('td');
+
+    $a.removeClass('stretched-link');
+
+    $td.wrapInner('<div class="position-relative p-3">');
+
+    $td
+        .addClass('p-0')
+        .find('.position-relative')
+        .prepend($aEmpty)
+    ;
+});
