305e9a4e30a137c3d0dd8606d183709d15d174e5
Amin HADRICH <mamin@rdimanager.com>
2021-11-04 14:59:06 +0100

    # use Tippy.js to show image in tooltip (on hover)

--- assets/js/popper.js
+++ assets/js/popper.js
@@ -1,6 +1,8 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
+import $ from 'jquery';
+
 document.addEventListener('DOMContentLoaded', () => {
     // Wrap disabled elements to make sure tooltip appears
     document.querySelectorAll('[title]').forEach(element => {
@@ -18,4 +20,25 @@ document.addEventListener('DOMContentLoaded', () => {
             instance.reference.removeAttribute('title');
         },
     });
+
+    $('[miniature]').mouseover(function(elem) {
+        let img_url = $(this).attr('href');
+        let img_fileName = $(this).text();
+        tippy(this, {
+            content: element => element.getAttribute('title'),
+            flipOnUpdate: true,
+            onShow(instance) {
+                // Create an image
+                const image = new Image();
+                image.style.display = 'block';
+                image.style.maxWidth  = '300px';
+                image.style.maxHeight  = '200px';
+                image.src = img_url;
+                image.title = img_fileName;
+                // Update the tippy content with the image
+                instance.setContent(image);
+            }
+        });
+    });
+
 });
