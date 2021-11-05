cdf2996182b9f4a38ad1d3d92b2d633038ff6eeb
Amin HADRICH <mamin@rdimanager.com>
2021-11-05 10:48:05 +0100

    # convert list_project to datatable # solve bug : show project name on hover

--- assets/js/datatable.js
+++ assets/js/datatable.js
@@ -1,3 +1,10 @@
 import './datatable/dataTables.css';
 import './datatable/dataTables';
+import $ from "jquery";
+import {detectedLocale} from "./translation";
 
+$('.table.datatable:not(.custom-datatable)').DataTable( {
+    language: {
+        url: detectedLocale === 'fr' ? "https://cdn.datatables.net/plug-ins/1.11.3/i18n/fr_fr.json" : null,
+    },
+} );
