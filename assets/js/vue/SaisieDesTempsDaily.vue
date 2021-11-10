89cb1249e3504e0cd9039edf3db9beeeb5c803b0
Amin HADRICH <mamin@rdimanager.com>
2021-11-10 15:27:54 +0100

    - Griser les deux colonne Samedi et Dimanche dans temps passé à la journée

--- assets/js/vue/SaisieDesTempsDaily.vue
+++ assets/js/vue/SaisieDesTempsDaily.vue
@@ -253,4 +253,8 @@ export default {
         width: 4.5em;
         white-space: nowrap;
     }
+    .saisie-des-temps-daily table tbody td:nth-last-child(-n+2),.saisie-des-temps-daily table thead th:nth-last-child(-n+2){
+        background-color: #788189;
+        border-color: #788189;
+    }
 </style>
