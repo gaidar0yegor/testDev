a7a911fdcf25964f092afb9c33000f013aa278ac
Amin HADRICH <mamin@rdimanager.com>
2021-11-29 12:32:19 +0100

    # fix word-break fait marquant

--- migrations/Version20211119123840.php
+++ migrations/Version20211119123840.php
@@ -20,7 +20,7 @@ final class Version20211119123840 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-//        $this->addSql('ALTER TABLE projet ADD color_code VARCHAR(10) NOT NULL');
+        $this->addSql('ALTER TABLE projet ADD color_code VARCHAR(10) NOT NULL');
     }
 
     public function down(Schema $schema): void
