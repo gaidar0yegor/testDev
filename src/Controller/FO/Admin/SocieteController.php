e064a5a4315ac21350568e1dd7d9284ee913485d
Amin HADRICH <mamin@rdimanager.com>
2021-12-09 16:17:34 +0100

    # code color societe

--- src/Controller/FO/Admin/SocieteController.php
+++ src/Controller/FO/Admin/SocieteController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller\FO\Admin;
 
+use App\Entity\Activity;
 use App\Entity\Fichier;
+use App\Entity\FichierProjet;
+use App\Entity\Projet;
+use App\File\FileHandler\ProjectFileHandler;
 use App\Form\AvatarType;
 use App\Form\SocieteType;
 use App\MultiSociete\UserContext;
+use App\ProjetResourceInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -79,5 +86,29 @@ class SocieteController extends AbstractController
         return $this->render('societe/edit_logo.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route(
+     *      "modifier/couleur",
+     *      name="app_fo_admin_societe_code_color_edit",
+     *      methods={"POST"}
+     * )
+     */
+    public function codeColorEdit(
+        Request $request,
+        EntityManagerInterface $em,
+        UserContext $userContext
+    ) {
+        $societe = $userContext->getSocieteUser()->getSociete();
+
+        if ($request->request->has('code_color')){
+            $societe->setColorCode($request->request->get('code_color'));
+            $em->persist($societe);
+            $em->flush();
+            return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+        } else {
+            return new JsonResponse(null, JsonResponse::HTTP_BAD_REQUEST);
+        }
     }
 }
---
+++ src/Twig/CodeColorSocieteExtension.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Twig;
+
+use App\MultiSociete\UserContext;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class CodeColorSocieteExtension extends AbstractExtension
+{
+    public const RDI_COLOR = '#ce352c';
+    public const BLACK_COLOR = '#000000';
+    public const WHITE_COLOR = '#ffffff';
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('apply_code_color_societe', [$this, 'applyCodeColorSociete']),
+        ];
+    }
+
+    public function applyCodeColorSociete(UserContext $userContext, string $cssAttribute) :string
+    {
+        $hexColor = $userContext->hasSocieteUser() ? $userContext->getSocieteUser()->getSociete()->getColorCode() : $this::RDI_COLOR;
+
+        switch ($cssAttribute){
+            case 'background-image':
+                return "background-image: linear-gradient(90deg, ".$this::RDI_COLOR." 65%, ".$hexColor." 100%);";
+            case 'background-color':
+                return "background-color: ".$hexColor.";color: ".($this->hexIsLight($hexColor) ? $this::BLACK_COLOR : $this::WHITE_COLOR);
+            default:
+                return '';
+        }
+    }
+
+    private function hexIsLight(string $hexColor)
+    {
+        $hex = str_replace( '#', '', $hexColor );
+
+        $c_r = hexdec( substr( $hex, 0, 2 ) );
+        $c_g = hexdec( substr( $hex, 2, 2 ) );
+        $c_b = hexdec( substr( $hex, 4, 2 ) );
+
+        $brightness = ( ( $c_r * 299 ) + ( $c_g * 587 ) + ( $c_b * 114 ) ) / 1000;
+
+        return $brightness > 155;
+    }
+}
