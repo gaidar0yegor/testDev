d325afb0a5881b3c7d1df0819327773afb6c9909
Amin HADRICH <mamin@rdimanager.com>
2021-12-17 14:22:30 +0100

    # update migration

--- migrations/Version20211217135108.php
+++ migrations/Version20211217135108.php
@@ -20,7 +20,16 @@ final class Version20211217135108 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DELETE FROM projet_participant WHERE role <> "PROJET_CDP" AND id IN ( SELECT projet_participant.id FROM projet_participant INNER JOIN societe_user ON projet_participant.societe_user_id = societe_user.id WHERE societe_user.enabled = 0 );');
+        $this->addSql('
+            DELETE FROM projet_participant
+            WHERE role <> "PROJET_CDP" AND id IN (
+                SELECT * FROM (
+                    SELECT projet_participant.id FROM projet_participant 
+                    INNER JOIN societe_user ON projet_participant.societe_user_id = societe_user.id 
+                    WHERE societe_user.enabled = 0
+                ) as ppid
+            );
+        ');
     }
 
     public function down(Schema $schema): void
