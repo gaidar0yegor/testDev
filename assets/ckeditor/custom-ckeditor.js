08fdac9afe2ae8069c4d94ce55c2ccca11e2460a
Amin HADRICH <mamin@rdimanager.com>
2022-02-10 11:48:04 +0100

    # add Scayt to spelling checker

--- assets/ckeditor/custom-ckeditor.js
+++ assets/ckeditor/custom-ckeditor.js
@@ -4,3 +4,5 @@ if ($(window).width() <= 992 && typeof CKEDITOR !== 'undefined'){
     CKEDITOR.config.removeButtons = 'Cut,Copy,Paste,PasteText,Find,Replace,Subscript,Superscript,SelectAll,SpecialChar,Smiley,ckeditor_wiris_formulaEditor';
 }
 
+CKEDITOR.config.scayt_autoStartup = true;
+
