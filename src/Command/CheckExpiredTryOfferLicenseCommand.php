8c0598fe1e57551a3e1148ab6f7e35da6499058d
Amin HADRICH <mamin@rdimanager.com>
2022-03-02 14:18:00 +0100

    # create Cron : to generate starter license if try offre is expirated

---
+++ src/Command/CheckExpiredTryOfferLicenseCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Societe;
+use App\License\Factory\StarterLicenseFactory;
+use App\License\LicenseService;
+use App\LicenseGeneration\LicenseGeneration;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CheckExpiredTryOfferLicenseCommand extends Command
+{
+    protected static $defaultName = 'app:check-expired-try-offer-license';
+
+    private LicenseService $licenseService;
+    private LicenseGeneration $licenseGeneration;
+    private StarterLicenseFactory $starterLicenseFactory;
+    private EntityManagerInterface $em;
+
+    public function __construct(
+        LicenseService $licenseService,
+        LicenseGeneration $licenseGeneration,
+        StarterLicenseFactory $starterLicenseFactory,
+        EntityManagerInterface $em
+    )
+    {
+        parent::__construct();
+
+        $this->licenseService = $licenseService;
+        $this->licenseGeneration = $licenseGeneration;
+        $this->starterLicenseFactory = $starterLicenseFactory;
+        $this->em = $em;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Vérifier et transformer les licences d\'essai expirées')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $societes = $this->em->getRepository(Societe::class)->findAll();
+
+        foreach ($societes as $societe) {
+            if ($this->licenseService->checkHasTryLicenseExpired($societe)) {
+
+                $io->info('Offre d\'essai expirée pour la société : ' . $societe->getRaisonSociale());
+
+                $license = $this->starterLicenseFactory->createLicense($societe);
+
+                $licenseContent = $this->licenseGeneration->generateLicenseFile($license);
+
+                $this->licenseService->storeLicense($licenseContent);
+
+                $societe->setProductKey($license->getProductKey());
+
+                $this->em->persist($societe);
+
+                $io->info('Une license Starter est générée pour la société : ' . $societe->getRaisonSociale());
+            }
+        }
+
+        $this->em->flush();
+
+        $io->success('Toutes les licences ont été vérifées.');
+
+        return Command::SUCCESS;
+    }
+}
