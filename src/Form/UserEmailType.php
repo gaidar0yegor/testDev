352af2eabb5409225f003e43f7aeb9231418dfe9
Amin HADRICH <mamin@rdimanager.com>
2022-03-14 16:33:56 +0100

    #add start date

--- src/Form/UserEmailType.php
+++ src/Form/UserEmailType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\SocieteUser;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,6 +16,14 @@ class UserEmailType extends AbstractType
         $builder
             ->add('invitationEmail', EmailType::class, [
                 'label' => 'Email',
+            ])
+            ->add('societeUserPeriods', CollectionType::class, [
+                'label' => false,
+                'entry_type' => SocieteUserPeriodType::class,
+                'required' => false,
+                'allow_add' => false,
+                'allow_delete' => false,
+                'by_reference' => false,
             ])
         ;
     }
