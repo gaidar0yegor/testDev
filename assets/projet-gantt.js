848248a2a40c1524ed08d1ccc548a1aca29a99f5
Amin HADRICH <mamin@rdimanager.com>
2022-04-11 10:44:11 +0200

    # change footer

--- assets/projet-gantt.js
+++ assets/projet-gantt.js
@@ -1,18 +1,10 @@
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
 import './js/dhtmlx-gantt/style.css';
-
-import './js/dhtmlx-gantt/icons/ic_collapse_all_24.png';
-import './js/dhtmlx-gantt/icons/ic_expand_all_24.png';
-import './js/dhtmlx-gantt/icons/ic_export_24.png';
-import './js/dhtmlx-gantt/icons/ic_users_24.png';
-import './js/dhtmlx-gantt/icons/ic_zoom_in.png';
-import './js/dhtmlx-gantt/icons/ic_zoom_out.png';
-import './js/dhtmlx-gantt/icons/ic_file_pdf.png';
-import './js/dhtmlx-gantt/icons/ic_file_png.png';
-import './js/dhtmlx-gantt/icons/ic_file_xlsx.png';
-import './js/dhtmlx-gantt/icons/ic_file_xml.png';
-
 import 'dhtmlx-gantt/codebase/dhtmlxgantt';
 import './js/dhtmlx-gantt/export_api';
 import './js/dhtmlx-gantt/projet-planning';
 import './js/dhtmlx-gantt/assign-users';
+
+// Import all icons
+const imagesContext = require.context('./js/dhtmlx-gantt/icons', true);
+imagesContext.keys().forEach(imagesContext);
