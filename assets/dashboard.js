9c5061717422abbc6ba652c2b8aee7e0aad14e59
Amin HADRICH <mamin@rdimanager.com>
2022-05-05 14:29:34 +0200

    # efficacite moyenne tableau de bord

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -8,5 +8,6 @@ import './js/dashboard/temps-saisis';
 import './js/dashboard/heures-par-projet';
 import './js/dashboard/recents-projets';
 import './js/dashboard/moi-vs-equipe';
+import './js/dashboard/projets-efficacite-moyenne';
 import './js/dashboard/projets-statuts';
 import './js/dashboard/projets-rdi-vs-non-rdi';
