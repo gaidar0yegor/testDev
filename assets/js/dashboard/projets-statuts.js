c6785359d7e42ab5b216738d5f76d7d3ea56ee4d
Amin HADRICH <mamin@rdimanager.com>
2022-08-23 15:25:35 +0200

    # color projet status

--- assets/js/dashboard/projets-statuts.js
+++ assets/js/dashboard/projets-statuts.js
@@ -19,9 +19,9 @@ const chart = c3.generate({
     },
     color: {
         pattern: [
-            '#ffc107',
+            '#1f77b4',
             '#28a745',
-            '#dc3545',
+            '#ffc107',
         ],
     },
 });
