4b6ec92abf840ad34dba12ad04858ae6b48d9039
Amin HADRICH <mamin@rdimanager.com>
2022-09-23 15:36:40 +0200

    # actualite carousel + filter

--- assets/js/multisociete/dashboard-consolide/recents-projets.js
+++ assets/js/multisociete/dashboard-consolide/recents-projets.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import {initOwlCarousel} from '../../owl-carousel';
+import {initSlickCarousel} from "../../slick-carousel/init-slick-carousel";
 import {dashboardContentsId} from '../utils';
 
 $(() => {
@@ -8,7 +8,7 @@ $(() => {
     }
 
     const $wrapper = $('#recents-projets');
-    const $cards = $('<div class="owl-carousel owl-theme">');
+    const $cards = $('<div class="slick-carousel">');
 
     const dashboardConsolide = $wrapper.data('dashboardConsolide');
 
@@ -26,24 +26,29 @@ $(() => {
                 activity.innerHTML = projet.activity.trim();
 
                 const $projet = $(`
-                    <div class="item card">
+                    <div class="item card-actualite" style="border: 1px solid ${projet.colorCode};border-left: 5px solid ${projet.colorCode};">
+                        <div class="card-side">
+                            <i class="fa fa-exclamation-circle" style="color: ${projet.colorCode};"></i>
+                        </div>
                         <div class="card-body">
-                          <h5 class="card-title">
-                                <span>${projet.societe} / ${projet.acronyme}</span>
-                                <span class="badge d-inline-block rounded-circle mt-1 float-right" style="background-color: ${projet.colorCode};width: 15px;height: 15px;"></span>
-                            </h5>
-                            <ul class="list-unstyled">${activity.innerText}</ul>
-                              <div class="card-footer">
+                            <div class="m-0 p-0">
+                                <h5 class="card-title">
+                                    <a href="javascript:;">${projet.societe} / ${projet.acronyme}</a>
+                                    <i class="fa fa-sm fa-circle" aria-hidden="true"></i>
                                     <small><i>${projet.datetime}</i></small>
-                                </div>
+                                </h5>
+                                <ul class="list-unstyled">${activity.innerText}</ul>
                             </div>
+                        </div>
                     </div>
-                `);
+                    `);
 
                 $cards.append($projet);
             });
 
-            initOwlCarousel($cards);
+            $(document).ready(function(){
+                initSlickCarousel($cards);
+            });
         })
     ;
 });
