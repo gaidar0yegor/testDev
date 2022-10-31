bd9010785932e930c738d99bb5d9f16271cfd212
Amin HADRICH <mamin@rdimanager.com>
2022-10-31 13:02:17 +0100

    # comming soon redirection

--- src/Controller/LabApp/CommingSoonController.php
+++ src/Controller/LabApp/CommingSoonController.php
@@ -13,6 +13,9 @@ class CommingSoonController extends AbstractController
      */
     public function index(): Response
     {
+        if (str_contains($this->getParameter('router.request_context.host'), 'afpa')){
+            return $this->redirectToRoute('lab_app_fo_multi_user_book_switch');
+        }
         return $this->render('_comming_soon.html.twig');
     }
 }
