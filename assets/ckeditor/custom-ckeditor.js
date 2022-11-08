e4a50803614ded431cca2726c8fc555a8bbc1b9e
Amin HADRICH <mamin@rdimanager.com>
2022-11-08 11:33:00 +0100

    # ckeditor latex equation

--- assets/ckeditor/custom-ckeditor.js
+++ assets/ckeditor/custom-ckeditor.js
@@ -4,11 +4,13 @@ import { detectedLocale } from "./../js/translation";
 if (typeof CKEDITOR !== "undefined") {
     if ($(window).width() <= 992) {
         CKEDITOR.config.removeButtons =
-            "Cut,Copy,Paste,PasteText,Find,Replace,Subscript,Superscript,SelectAll,SpecialChar,Smiley,ckeditor_wiris_formulaEditor";
+            "Cut,Copy,Paste,PasteText,Find,Replace,Subscript,Superscript,SelectAll,SpecialChar,Smiley,mathjax";
     }
 
     CKEDITOR.config.scayt_autoStartup = true;
     CKEDITOR.config.scayt_sLang = detectedLocale + "_" + detectedLocale.toUpperCase();
+    CKEDITOR.config.mathJaxLib = '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS_HTML';
+
     CKEDITOR.timestamp = (Math.random() + 1).toString(36).substring(7);
 
     $('.container.main-container').append(`<div class="ckeditor-preview-popup rdi-popup bg-modal" style="display: none;">
