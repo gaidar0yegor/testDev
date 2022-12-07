050440498b4a9b97d7309b901e245806c3709a51
LouisPLN <louispoulinpro@gmail.com>
2022-12-07 11:21:09 +0100

    Dashboard export

--- assets/js/html-to-pdf.js
+++ assets/js/html-to-pdf.js
@@ -116,7 +116,7 @@ function exportToPdf($button, initData)
                         // Footer Left 
                         pdf.text(footerLeft, 0.2, pageHeight - 0.2, { baseline: 'bottom' });
                         // Header Right
-                        pdf.addImage(headerRight, "PNG", pageWidth - 1.2, 0.1, 0.9, 0.5, { baseline: 'top' });
+                        pdf.addImage(headerRight, "PNG", pageWidth - 1.2, 0.1, 0.8, 0.3, { baseline: 'top' });
                         // Footer Right
                         pdf.text(footerRight, pageWidth - 2, pageHeight - 0.2, { baseline: 'bottom' });
                     }
