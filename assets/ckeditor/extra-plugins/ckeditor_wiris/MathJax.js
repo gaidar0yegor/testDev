96c6fc5da0609142572c62564ebce8acf5d065e0
LouisPLN <louispoulinpro@gmail.com>
2023-03-22 12:16:05 +0100

    Update MathJax & fix project export

--- assets/ckeditor/extra-plugins/ckeditor_wiris/MathJax.js
+++ assets/ckeditor/extra-plugins/ckeditor_wiris/MathJax.js
@@ -1,17 +1,18 @@
 (function () {
-  var newMathJax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.0.0/MathJax.js';
-  var oldMathJax = 'cdn.mathjax.org/mathjax/2.0-latest/MathJax.js';
+  var newMathJax =
+    "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+  var oldMathJax = "cdn.mathjax.org/mathjax/2.0-latest/MathJax.js";
 
   var replaceScript = function (script, src) {
     //
     //  Make redirected script
     //
-    var newScript = document.createElement('script');
-    newScript.src = newMathJax + src.replace(/.*?(\?|$)/, '$1');
+    var newScript = document.createElement("script");
+    newScript.src = newMathJax + src.replace(/.*?(\?|$)/, "$1");
     //
     //  Move onload and onerror handlers to new script
     //
-    newScript.onload = script.onload; 
+    newScript.onload = script.onload;
     newScript.onerror = script.onerror;
     script.onload = script.onerror = null;
     //
@@ -29,7 +30,7 @@
     //
     //  Issue a console warning
     //
-  }
+  };
 
   if (document.currentScript) {
     var script = document.currentScript;
@@ -39,10 +40,10 @@
     // Look for current script by searching for one with the right source
     //
     var n = oldMathJax.length;
-    var scripts = document.getElementsByTagName('script');
+    var scripts = document.getElementsByTagName("script");
     for (var i = 0; i < scripts.length; i++) {
       var script = scripts[i];
-      var src = (script.src || '').replace(/.*?:\/\//,'');
+      var src = (script.src || "").replace(/.*?:\/\//, "");
       if (src.substr(0, n) === oldMathJax) {
         replaceScript(script, src);
         break;
